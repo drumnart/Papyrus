f654e51865e232872bc88f73c75025a08a38cb2e
drumnart <drumnart@gmail.com>
2017-03-30 20:00:28 +0300

    initial configuration

---
+++ Papyrus/Papyrus.h
@@ -0,0 +1,19 @@
+//
+//  Papyrus.h
+//  Papyrus
+//
+//  Created by Sergey Gorin on 30.03.17.
+//  Copyright © 2017 ToBox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Papyrus.
+FOUNDATION_EXPORT double PapyrusVersionNumber;
+
+//! Project version string for Papyrus.
+FOUNDATION_EXPORT const unsigned char PapyrusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Papyrus/PublicHeader.h>
+
+
